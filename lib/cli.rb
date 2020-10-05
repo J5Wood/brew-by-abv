bfbbfefdd9bbedd9f57efe8518e6022444255008
J5WOOD <js.wood206@gmail.com>
2020-10-05 10:30:12 -0600

    Started building CLI, added start function to bin

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,1 +1,13 @@
 require_relative '../environment'
+
+class CLI
+
+    def start
+        puts ""
+        puts "Welcome to find by ABV!"
+        puts ""
+        puts "You can find beers to brew based on the ABV% you're looking for."
+        puts ""
+    end
+
+end
